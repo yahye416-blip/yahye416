5f10a6774ff8646dc57d7410df7eb3bbed730796
yahye416 <abdilaahi.416@gmail.com>
2025-10-15 15:15:57 +0100

    removed my practice files

--- javascript/file.js
+++ javascript/file.js
@@ -1,16 +1,1 @@
-function   myFunction() {
-    document.getElementById("demo").innerHTML = "waryaa naga amus";
-}
-function  spanfunction() {  
-    document.getElementById("none").style.display = "none";
-}
-function   showfunction() { 
-    document.getElementById("show").style.display = "block";
-}
-function   divfunction() { 
-    document.getElementById("myDIV").style.display = "inline";
-}
-function   trialFunction() {
-    var fname = "yahye";
-    document.getElementById("trial").innerHTML = "asc"+" "+ fname;
-}
+console.log("Yahye Hashi");
