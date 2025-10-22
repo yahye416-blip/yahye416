ad62f62694728055cb025cbb6a0e1410c975ca24
yahye416 <abdilaahi.416@gmail.com>
2025-10-22 20:32:47 +0100

    modified some files

--- fastTrack_Academy/javascript/javascript_practice_1.js
+++ fastTrack_Academy/javascript/javascript_practice_1.js
@@ -1,12 +1,15 @@
 
-var firstname = ("Yahye");
-var greeting = ("Warya");
+ var firstname = ("Yahye");   //string
+ var greeting = ("Warya");  //string
 console.log(greeting + " " + firstname);
 var lastname = ("Hashi");
-console.log("sorry");
-console.log("I meant Hello " + " " + firstname + " " + lastname);
-var age = 26;
+console.log("Hello " + " " + firstname + " " + lastname);
+var age = 26;   //number
 console.log("You are " + age + " years old");
 var year = 2025;
 console.log("You were born in " + (year - age));
 var country = ("England");
+console.log(age *age) // prints 676
+console.log(firstname * greeting) // prints NaN
+var wearsGlasses = true;  //boolean
+console.log(wearsGlasses);   // prints true
